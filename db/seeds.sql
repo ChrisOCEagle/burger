ba423702aa1a1be492b8aa87b2d32096e1a31095
Siberianeagle5 <christopherdavyydoconnell.com>
2018-11-24 17:45:58 -0500

    added a seeds file and added entries into my db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bleucheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Mushroom and Swiss Burger");
