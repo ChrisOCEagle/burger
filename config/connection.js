19623457ddfd335e64e5ff6c1135fffd5e680877
ChrisOCEagle <christopherdavyydoconnell@yahoo.com>
2019-03-21 23:20:58 -0400

    almost done with the burger app!

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 var mysql = require("mysql");
 
 // create the connection to the mysql database
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burger_db"
+  });
+}
 
 // connect the database connection to the server
 connection.connect(function(err) {
